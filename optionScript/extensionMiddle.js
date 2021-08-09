4ec44a9915194f68a37c0bdafecf94f9bba13a60
lijianhao <lijianhao759@163.com>
2021-08-09 12:04:50 +0800

    自定义 fs 操作, 防止依赖不存在

--- optionScript/extensionMiddle.js
+++ optionScript/extensionMiddle.js
@@ -1,5 +1,4 @@
-const {program} = require('commander')
-const {removeSync, copySync} = require('fs-extra')
+const {copyFileSync, rmSync, rmdirSync, readdirSync, mkdirSync, existsSync} = require('fs')
 const {join, dirname} = require('path')
 const dirFolder = dirname(__dirname) // 代表根目录
 const {getBaseConfig, changBaseConfig} = require('./assetEvent')
@@ -18,21 +17,47 @@ function changExtension(extension){
     changBaseConfig(content.replace(reg, entryName))
 }
 
+// 复制源文件夹 -> 目标文件夹
+function copyDir(src, target){
+    // 判断 路径是否存在，不存在 创建 目录
+    if(!existsSync(target)) mkdirSync(target)
+    const srcFileName = readdirSync(src)
+    const srcFilePathArray = srcFileName.map( path =>{
+        return join(src, path)
+    })
+    const targetFilePathArray =  srcFileName.map( path =>{
+        return join(target, path)
+    })
+    srcFilePathArray.forEach((srcPath, index)=>{
+        copyFileSync(srcPath, targetFilePathArray[index])
+    })
+}
+
+// 删除
+function remove(path){
+    // 不存在 返回
+    if(!existsSync(path)) return
+    // 递归目录删除 / 删除文件
+    rmSync(path, {force: true, recursive: true})
+    // 如果 还存在，那么是 空目录
+    if(existsSync(path)) rmdirSync(path)
+}
+
 module.exports = (next)=>(argv)=>{
     const {extension} = argv
     // 更改 入口 扩展名
     changExtension(extension)
     // 删除 原src文件夹
-    removeSync(srcPath)
+    remove(srcPath)
     // 删除 tsconfig
-    removeSync(tsConfigRootPath)
+    remove(tsConfigRootPath)
     // 获取模板文件夹路径
     const copyFloder = join(templatePaht, `/${extension}`)
-    // 将模板文件夹 内容 进行复制
-    copySync(copyFloder, srcPath)
+    copyDir(copyFloder, srcPath)
+    // copySync(copyFloder, srcPath)
     // 如果是ts 将模板文件夹内的 tsconfig.json 移动 至 根目录
     if(extension === 'ts'){
-        copySync(tsConfigStorePath, tsConfigRootPath)
+        copyFileSync(tsConfigStorePath, tsConfigRootPath)
     }
     next(argv)
 }
